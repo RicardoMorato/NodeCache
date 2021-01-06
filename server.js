3dd8f5f652e4c62ff50979306a907475e92821f5
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2021-01-06 16:32:14 -0300

    feat: add redis connection through docker-compose

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const axios = require('axios');
 const redis = require('redis');
 
+const redisClient = require('./redis-client');
+
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 // Connecting to the local instance of redis
 const client = redis.createClient(process.env.REDIS_URL);
@@ -18,28 +20,23 @@ app.get('/recipe/:foodItem', async (req, res) => {
     const { foodItem } = req.params;
 
     // Check the redis store for the data first
-    client.get(foodItem, async (err, recipe) => {
-      if (recipe) {
-        return res.status(200).send({
-          error: false,
-          message: `Recipe for ${foodItem} from the cache`,
-          data: JSON.parse(recipe),
-        });
-      } else { // When data is not found in the cache, we should make a request to the server
-        const recipe = await axios.get(`http://www.recipepuppy.com/api/?q=${foodItem}`);
+    const data = await redisClient.getAsync(foodItem)
 
-        // Save the record in the cache for subsequent requests
-        client.setex(foodItem, 1440, JSON.stringify(recipe.data.results));
-
-        // Return the result to the client
-        return res.status(200).send({
-          error: false,
-          message: `Recipe for ${foodItem} from the server`,
-          data: recipe.data.results,
-        });
-      }
-    })
-
+    if (data) {
+      return res.status(200).send({
+        error: false,
+        message: `Recipe for ${foodItem} from the cache`,
+        data: JSON.parse(data),
+      })
+    } else {
+      const recipe = await axios.get(`http://www.recipepuppy.com/api/?q=${foodItem}`);
+      await redisClient.setAsync(foodItem, 1440, JSON.stringify(recipe.data.results));
+      return res.status(200).send({
+        error: false,
+        message: `Recipe for ${foodItem} from the server`,
+        data: recipe.data.results,
+      });
+    }
   } catch (err) {
     console.log(err);
   }
